3a01b6a235e6cc526920ee37d7792b9d018b4953
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-03-08 16:27:32 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import "./font-awesome-4.7.0/css/font-awesome.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <nav className="menu">
+        <label className="logo">JorgePlasencia</label>
+        <ul className="menu_items">
+          <li className="active">
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Servicios</a>
+          </li>
+          <li>
+            <a href="#">Acerca</a>
+          </li>
+          <li>
+            <a href="#">Portafolio</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+        <span className="btn_menu">
+          <i className="fa fa-bars"></i>
+        </span>
+      </nav>
     </div>
   );
 }
